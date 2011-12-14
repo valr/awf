cb70e4d1a999360fd0b20a8537409fa7789d21ca
Valère Monseur <valere.monseur@ymail.com>
2011-12-14 22:13:22 +0100

    ensure README exists before autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# Avoid autoreconf error for missing README file
-touch README
+# Ensure README file exists
+if test ! -e README; then
+  cp README.md README
+fi
 
 # Reconfigure all
 autoreconf -f -i
 
-# Check minimum required version
-# if which gtkdoc-depscan &> /dev/null; then
-#   echo "Minimum required version:"
-#   gtkdoc-depscan --book=gtk * | cut -d ' ' -f 2  | sort | uniq
-# fi
+# Check minimum required gtk version
+if which gtkdoc-depscan &> /dev/null; then
+  echo "Minimum required version:"
+  gtkdoc-depscan --book=gtk * | cut -d ' ' -f 2  | sort | uniq
+fi
